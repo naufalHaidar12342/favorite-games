bcb50b88f32d84e7ab6419d9cad7bce9eaba358c
naufalHaidar12342 <naufalhaidar12342@gmail.com>
2024-04-09 14:54:43 +0700

    add google html tag for search indexing

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,10 @@ import { jetBrainsMono } from "@/app/libraries/fonts";
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<meta
+				name="google-site-verification"
+				content="Y3gt82__7lVPhqwq2pva7k_4ndiVrxXiksHBGwt9W-c"
+			/>
 			<body className={jetBrainsMono.className}>{children}</body>
 		</html>
 	);
